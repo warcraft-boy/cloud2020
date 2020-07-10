768bab440133d4d9d425ba0b62907b0dbbd5adcb
chenjianwen <ChenJevin@163.com>
2020-07-10 17:17:29 +0800

    注释

--- cloud-consumer-order80/src/main/java/com/atguigu/myrule/MySelfRule.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/myrule/MySelfRule.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * @Description: 定义ribbon的负载均衡策略，有RoundRobinRule轮询，这里用的是RandomRule随机
+ * 由于ribbon默认的是轮询策略，想要修改自定义，官网建议不要被@ComponentScan注解扫描到
+ * 因为启动类@SpringBootApplication注解里面有@ComponentScan注解，所以自定义配置类不要放在和启动类同级包下面
  * @Author chenjianwen
  * @Date 2020/7/10
  **/
