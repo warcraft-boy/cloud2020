abc28546b6cd6c9355d20844f5034e4663e43c34
chenjianwen <ChenJevin@163.com>
2020-07-10 17:05:01 +0800

    1

--- cloud-consumer-order80/src/main/java/com/atguigu/myrule/MySelfRule.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/myrule/MySelfRule.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * @Description: 定义ribbon的负载均衡策略，有RoundRobinRule轮询，这里用的是RandomRule随机 
+ * @Description: 定义ribbon的负载均衡策略，有RoundRobinRule轮询，这里用的是RandomRule随机
  * @Author chenjianwen
  * @Date 2020/7/10
  **/
