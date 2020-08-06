e7ae08b7a39df576d9e60e3106a4f4d1b3863163
chenjianwen <ChenJevin@163.com>
2020-08-06 14:45:41 +0800

    sentinel流控规则

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/SentinelMain8401.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description:
+ * @Author chenjianwen
+ * @Date 2020/8/3
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SentinelMain8401 {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelMain8401.class, args);
+    }
+}
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/myHandler/CustomerBlockHandler.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud.myHandler;
+
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.atguigu.springcloud.entities.CommonResult;
+
+/**
+ * @Description:
+ * @Author chenjianwen
+ * @Date 2020/8/3
+ **/
+public class CustomerBlockHandler {
+
+    public static CommonResult handlerException(BlockException exception) {
+        return new CommonResult(444, "客户自定义，global handlerException---1");
+    }
+
+    public static CommonResult handlerException2(BlockException exception) {
+        return new CommonResult(444, "客户自定义，global handlerException---2");
+    }
+}
